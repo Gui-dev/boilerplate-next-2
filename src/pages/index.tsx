40d5d49a0d9c4c7d2b871c8ed794e6934a62289f
Gui-dev <adrianon10@gmail.com>
2021-09-02 19:38:22 -0300

    feat: add Main component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+import { Main } from 'components/Main'
 
 export default function Home () {
   return (
-    <Title>My page</Title>
+    <Main />
   )
 }
