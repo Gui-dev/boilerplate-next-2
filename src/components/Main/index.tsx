9d0ffc3a87ddac363e8ddfc23178c28db74c9a2b
Gui-dev <adrianon10@gmail.com>
2021-09-02 19:31:27 -0300

    feat: add structure

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,16 @@
-import { Container } from './style'
+import { Container, Logo, Title, Description, Illustration } from './style'
+
+// import {} from ''
 
 export const Main = () => {
   return (
     <Container>
-      <h1>React Avançado</h1>
+      <Logo src="/images/logo.svg" alt="Imagem de um átomo e React avançado do lado" title="React Avançado"/>
+      <Title>React Avançado</Title>
+      <Description>
+        Typescript, ReactJS, NextJS e Styled Components
+      </Description>
+      <Illustration src="/images/hero-illustration.svg" alt="Um desenvolvedor de frente para a tela com código"/>
     </Container>
   )
 }
