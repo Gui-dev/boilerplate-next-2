54abe423accb4f3ff741a7bd7e5a7f4c44018a71
Gui-dev <adrianon10@gmail.com>
2021-09-02 19:33:30 -0300

    style: add page styles

--- src/components/Main/style.ts
+++ src/components/Main/style.ts
@@ -1,5 +1,33 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Container = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  color: #FFF;
+  height: 100%;
+  width: 100%;
+  background-color: #191919;
+`
 
+export const Logo = styled.img`
+  width: 25rem;
+  margin-bottom: 2rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 700;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
+
+export const Illustration = styled.img`
+  margin-top: 3rem;
+  width: min(30rem, 100%);
 `
