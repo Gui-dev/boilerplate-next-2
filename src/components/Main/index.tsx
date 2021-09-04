d228463bc8c914e1302b7e12df1fb63c0d38abec
Gui-dev <adrianon10@gmail.com>
2021-09-04 03:42:45 -0300

    refactor: add new content

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,14 +1,13 @@
 import { Container, Logo, Title, Description, Illustration } from './style'
 
-// import {} from ''
-
-export const Main = () => {
+export const Main = ({ title = 'React Avançado' }) => {
   return (
     <Container>
       <Logo src="/images/logo.svg" alt="Imagem de um átomo e React avançado do lado" title="React Avançado"/>
-      <Title>React Avançado</Title>
+      <Title>{ title }</Title>
       <Description>
-        Typescript, ReactJS, NextJS e Styled Components
+        Typescript, ReactJS, NextJS, Styled Components,<br />
+        Jest, Testing Library React, Storybook, Husky
       </Description>
       <Illustration src="/images/hero-illustration.svg" alt="Um desenvolvedor de frente para a tela com código"/>
     </Container>
